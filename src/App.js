2f5f6e5e9404bd79c1a7aa315009edf8595ed88c
Jasurbek <jasurisoqov685@gmail.com>
2022-05-13 09:06:40 +0500

    create start quiz app

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
 import React from "react";
-import { useState } from "react";
+import "./app.css";
 
 const App = () => {
-  const [count, setCount] = useState({
-    firstName: "",
-    lastName: "",
-    yourEmail: "",
-  });
-
   return (
-    <div>
-      <input
-        style={{ padding: "5px", margin: "10px 0" }}
-        type={"text"}
-        value={count.firstName}
-        onChange={(e) => setCount({ ...count, firstName: e.target.value })}
-      />
-      <br />
-      <input
-        style={{ padding: "5px", margin: "10px 0" }}
-        type={"text"}
-        value={count.lastName}
-        onChange={(e) => setCount({ ...count, lastName: e.target.value })}
-      />
-      <br />
-      <input
-        style={{ padding: "5px", margin: "10px 0" }}
-        type={"text"}
-        value={count.yourEmail}
-        onChange={(e) => setCount({ ...count, yourEmail: e.target.value })}
-      />
-      <br />
-      <h3>First name: {count.firstName}</h3>
-      <h3>Last name: {count.lastName}</h3>
-      <h3>Your Email: {count.yourEmail}</h3>
-      {backInput()}
+    <div className="app">
+      <div className="main">Main</div>
+      <div className="pyramid">Money</div>
     </div>
   );
 };
