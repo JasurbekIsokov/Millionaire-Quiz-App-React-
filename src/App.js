7171bc72147da05dc022ad6a5bfd2e93498251b9
Jasurbek <jasurisoqov685@gmail.com>
2022-05-13 09:19:32 +0500

    moneyListItem

--- src/App.js
+++ src/App.js
@@ -5,7 +5,26 @@ const App = () => {
   return (
     <div className="app">
       <div className="main">Main</div>
-      <div className="pyramid">Money</div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          <li className="moneyListItem active">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$400</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$400</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$400</span>
+          </li>
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$400</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
